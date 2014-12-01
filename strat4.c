289e23edd193bbce92760dca067ca41c609bf8bb
Greg Pohodich <greg@pohodich.com>
2014-11-30 20:39:19 -0500

    Create strat4.c

---
+++ strat4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#include "gofish.h"
+#include "extern.h"
+
+
+/* Strategy-4
+ *   come up with your own technique for selecting cards
+ */
+int Strategy4(int player)
+{
+    int card;
+
+    // ... put code here ...
+
+    return(card);
+}
