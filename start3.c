e644819c9ea292aa68fb41d675a435fe19dd4c8b
Greg Pohodich <greg@pohodich.com>
2014-11-30 20:39:07 -0500

    Create start3.c

---
+++ start3.c
@@ -0,0 +1,25 @@
+/*
+ * This program contains a strategy module for a
+ * Go Fish program which prioritizes the greatest
+ * grouping of cards as the next card to be asked
+ */
+
+#include <stdio.h>
+
+#include "gofish.h"
+#include "extern.h"
+
+/* Strategy-3
+ *    select a card based on a prioritization scheme
+ *    first check for groups of 3 cards, ask one of these
+ *    then check for single cards, ask this
+ *    and then anything else (groups of 2), ask this
+ */
+int Strategy3(int player)
+{
+    int card;
+
+    // ... put code here ...
+
+    return(card);
+}
