1602a81eb259ce4ad38f7042ecb0e868af36aa7a
Christopher Conklin <cgc.ind@gmail.com>
2014-12-05 14:35:47 -0500

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -1,3 +1,7 @@
+/* Christopher Conklin
+   12/5/2014
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,19 +18,15 @@
 // Display the results
 
 
-#define         TRIAL_TIMES         3        // how many times to run each strategy against each other
-#define         NUM_STRATEGIES      4
+#define         TRIAL_TIMES         25        // how many times to run each strategy against each other
+#define         NUM_STRATEGIES      6
 #define         USR_STRATEGY        1
-
-void TestDrawCard(int);
-void TestTurn(int);
-int Trial(int, int);
-void TestCheckForGroup(int, int);
 
 void testGame(void)
 {
 	/*Create index variable*/
 	int index = 0;
+	char *name;
 
 	/*Create integer variable for game return*/
 	int winner = 0;
@@ -38,13 +38,17 @@ void testGame(void)
 	int strategy1 = 0, strategy2 = 0;
 
 	/*Create array for strategy ID numbers*/
-	int strategies[NUM_STRATEGIES] = { 0, 2, 3, 4 };
+	int strategies[NUM_STRATEGIES] = { 0, 2, 3, 4, 5, 6};
 
 	/*Create array for win, lose,and overall count*/
-	int wins[NUM_STRATEGIES] = { 0, 0, 0, 0 };
-	int loses[NUM_STRATEGIES] = { 0, 0, 0, 0 };
-	int overall[NUM_STRATEGIES] = { 0, 0, 0, 0 };
+	int wins[NUM_STRATEGIES];
+	int loses[NUM_STRATEGIES];
+	int overall[NUM_STRATEGIES];
 
+	/*Reset arrays*/
+	memset(wins, 0, NUM_STRATEGIES);
+	memset(loses, 0, NUM_STRATEGIES);
+	memset(overall, 0, NUM_STRATEGIES);
 
 	/*For twenty five random decks*/
 	for (randindex = 0; randindex < TRIAL_TIMES; randindex++)
@@ -88,11 +92,31 @@ void testGame(void)
 	/*Print Results*/
 	for (index = 0; index < NUM_STRATEGIES; index++)
 	{
-		printf("Strategy%d:\tWins: %d\tLoses: %d\tOverall: %d\n",strategies[index], wins[index], loses[index], overall[index]);
+		/*Switch names, look at index*/
+		switch (index)
+		{
+		case 0:	/*First Basic Strategy*/
+			name = "Basic Example "; break;
+		case 1: /*Zach's strategy*/
+			name = "Zack Cross    "; break;
+		case 2: /*Professor Strom simple*/
+			name = "Prof. Strom   "; break;
+		case 3: /*Dom's strategy*/
+			name = "Dom Risaliti  "; break;
+		case 4: /*Professor Strom Advanced*/
+			name = "Expert Strom  "; break;
+		case 5: /*Expert student */
+			name = "Expert Student"; break;
+		case 6: /*Additional Methods */
+			name = "              "; break;
+		}
+		/*Print strategy results*/
+		printf("%s:\tWins: %d\tLoses: %d\tOverall: %d\n",name, wins[index], loses[index], overall[index]);
 	}
-	getchar();
+
+	/*Wait for character before exit*/
+	getch();
     exit(0);
 }
-
 
 
